2c9e8c7fd5083301c342201069227c85d02420d2
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-06-17 23:13:47 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,42 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
-	end
-
-	def update
+    @post = Post.new(post_params(:title, :description))
+    @post.save
+    redirect_to post_path(@post)
+  end
+ 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params(:title))
+    redirect_to post_path(@post)
+  end
+  
+  def edit
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
 	end
-
-	def edit
-	  @post = Post.find(params[:id])
-	end
+ 
+  private
+ 
+ 
+# We pass the permitted fields in as *args;
+# this keeps `post_params` pretty dry while
+# still allowing slightly different behavior
+# depending on the controller action. This
+# should come after the other methods
+ 
+  def post_params(*args)
+    params.require(:post).permit(*args)
+  end
+  
+  
 end
+
+
+
+
+#require is more restrictive 
+
+#require means that params that get passed from the uer MUST contain a key called post. Otherwise it fails and the user gets an error. 
+
+#permit is looser. It means that params hash may have whatever keys are in it. 
